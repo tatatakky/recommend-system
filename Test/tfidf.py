d84e5c76b20c620b059a2436d11cca54c24a67ea
tatatakky <s1240232@gmail.com>
2018-07-24 23:32:23 +0900

    [modify] easy to see this file

--- Test/tfidf.py
+++ Test/tfidf.py
@@ -1,8 +1,13 @@
 def calc_tfidf(sentence, sentence_all):
     from tf import calc_tf
     from idf import calc_idf
+
     tf, word_list = calc_tf(sentence)
+    print("---------tf---------\n{}".format(tf))
+
     idf = calc_idf(sentence_all, word_list)
+    print("---------idf---------\n{}".format(idf))
+
     import numpy as np
     tfidf = list(np.array(tf) * np.array(idf))
     return tfidf, word_list
