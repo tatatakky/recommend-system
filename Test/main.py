42d7acab64e82d6c3412bbb447b449d1d77d9bc3
tatatakky <s1240232@gmail.com>
2018-07-24 13:49:28 +0900

    [add] normalization func

--- Test/main.py
+++ Test/main.py
@@ -1,15 +1,14 @@
+from tfidf import calc_tfidf
+from make_data import data_maker
+from data_for_normalization import make_normalization_data
+from normalization import normalize
 def main():
-    from tfidf import calc_tfidf
-    from make_data import data_maker
-    from data_for_normalization import make_normalization_data
-
     data = data_maker()
-
-    word_for_normalization, list_for_normalization = make_normalization_data(data)
 
     for i in range(len(data)):
         tfidf, word_list_i = calc_tfidf(data[i], data)
-
+        word_for_normalization, list_for_normalization = make_normalization_data(data)
+        print(normalize(word_list_i, tfidf, word_for_normalization,list_for_normalization))
 
 if __name__ == '__main__':
     main()
