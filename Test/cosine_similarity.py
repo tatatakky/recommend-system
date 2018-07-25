6f81082770dd9a05333821e693e59f05af600f3a
tatatakky <s1240232@gmail.com>
2018-07-26 01:58:54 +0900

    [delete] refactoring -> delete no-used

--- Test/cosine_similarity.py
+++ Test/cosine_similarity.py
@@ -1,7 +1,6 @@
 def calc_cosine_similarity(normalization):
     import numpy as np
     similarity = []
-    norm_len = len(normalization[0])
     for i in range(1,len(normalization)):
         a,b = np.array(normalization[0]),np.array(normalization[i])
         similarity.append(np.dot(a,b)/(np.linalg.norm(a) * np.linalg.norm(b)))
