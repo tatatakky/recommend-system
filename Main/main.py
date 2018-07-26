daecec4a49357e1ef972f44a8143a1c05ff415ac
tatatakky <s1240232@gmail.com>
2018-07-26 19:09:37 +0900

    [modify] read  eng_data -> jp_data

---
+++ Main/main.py
@@ -0,0 +1,20 @@
+from tfidf import calc_tfidf
+from make_data_japanese import make_jp_data
+from data_for_normalization import make_normalization_data
+from normalization import normalize
+from cosine_similarity import calc_cosine_similarity
+def main():
+    normalization = []
+
+    data = make_jp_data()
+
+    for i in range(len(data)):
+        tfidf, word_list_i = calc_tfidf(data[i], data)
+        word_for_normalization, list_for_normalization = make_normalization_data(data)
+        normalization.append(normalize(word_list_i, tfidf, word_for_normalization,list_for_normalization))
+
+    print("\n\n===============similarity===============")
+    print(calc_cosine_similarity(normalization))
+
+if __name__ == '__main__':
+    main()
