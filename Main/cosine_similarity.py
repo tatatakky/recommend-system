f30554e56d2de10191a593d20231cf2157f768cc
tatatakky <s1240232@gmail.com>
2018-07-26 19:14:35 +0900

    [add] data

--- Main/cosine_similarity.py
+++ Main/cosine_similarity.py
@@ -1,7 +1,7 @@
 def calc_cosine_similarity(normalization):
     import numpy as np
-    similarity = []
+    similarity_data = []
     for i in range(1,len(normalization)):
         a,b = np.array(normalization[0]),np.array(normalization[i])
-        similarity.append(np.dot(a,b)/(np.linalg.norm(a) * np.linalg.norm(b)))
-    return similarity
+        similarity_data.append(np.dot(a,b)/(np.linalg.norm(a) * np.linalg.norm(b)))
+    return similarity_data
